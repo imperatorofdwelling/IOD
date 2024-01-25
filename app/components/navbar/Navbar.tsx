e5da9dcfc7d7e7e40138fc17d0a1c4d5dae24def
Zheka <zheseleznev@yandex.ru>
2024-01-25 23:29:23 +0300

    Create navbar component

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,10 +1,19 @@
 import Container from '../Container'
+import Search from '../Search'
+import UserMenu from '../UserMenu'
+import Logo from './Logo'
 
 const Navbar = () => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
-        <Container>some text</Container>
+        <Container>
+          <div className="flex items-center justify-between gap-3 md:gap-0">
+            <Logo />
+            <Search />
+            <UserMenu />
+          </div>
+        </Container>
       </div>
     </div>
   )
