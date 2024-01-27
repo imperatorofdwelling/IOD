aa1c25503911a008156e4affcb384759bd07ea82
Zheka <zheseleznev@yandex.ru>
2024-01-27 18:41:34 +0300

    Add prisma and model User

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -102,6 +102,7 @@ const Modal: React.FC<IModal> = ({
                     onClick={handleSubmit}
                   />
                 </div>
+                {footer}
               </div>
             </div>
           </div>
--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -4,11 +4,14 @@ import useRegisterModal from '@hooks/useRegisterModal'
 import axios from 'axios'
 import { AiFillGithub } from 'react-icons/ai'
 import { FcGoogle } from 'react-icons/fc'
+import { FaYandex } from 'react-icons/fa'
 import { useCallback, useState } from 'react'
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 import Modal from './Modal'
 import Heading from '../Heading'
 import Input from '../inputs/Input'
+import toast from 'react-hot-toast'
+import Button from '../Button'
 
 const RegisterModal = () => {
   const registeModal = useRegisterModal()
@@ -35,7 +38,7 @@ const RegisterModal = () => {
         registeModal.onClose()
       })
       .catch((error) => {
-        console.log(error)
+        toast.error('Что-то пошло не так')
       })
       .finally(() => {
         setIsLoading(false)
@@ -76,6 +79,41 @@ const RegisterModal = () => {
     </div>
   )
 
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Войти с помощью Yandex"
+        icon={FaYandex}
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label="Войти с помощью Google"
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label="Войти с помощью Github"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div className="text-neutral-500 text-center mt-4 font-light">
+        <div className="flex justify-center flex-row items-center gap-2">
+          <div>Уже зарегестрированы?</div>
+          <div
+            className="text-black cursor-pointer hover:underline transition"
+            onClick={registeModal.onClose}
+          >
+            Войти
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
   return (
     <Modal
       disabled={isLoading}
@@ -85,6 +123,7 @@ const RegisterModal = () => {
       onClose={registeModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
+      footer={footerContent}
     />
   )
 }
