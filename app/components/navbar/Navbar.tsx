37431a760da534a0b8a8c77b375cd8388e9dbd5c
Zheka <zheseleznev@yandex.ru>
2024-01-28 21:26:18 +0300

    Craete git current user function

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,9 +1,15 @@
+import { User } from '@prisma/client'
 import Container from '../Container'
 import Search from '../Search'
 import UserMenu from '../UserMenu'
 import Logo from './Logo'
 
-const Navbar = () => {
+interface INavbar {
+  currentUser?: User | null
+}
+
+const Navbar: React.FC<INavbar> = ({ currentUser }) => {
+  console.log({ currentUser })
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
