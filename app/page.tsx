3dd69c67ff70a8ca86452b289c139e254cc7c1d8
Zheka <zheseleznev@yandex.ru>
2024-02-05 21:34:29 +0300

    Fix

--- app/page.tsx
+++ app/page.tsx
@@ -5,34 +5,44 @@ import Container from './components/Container'
 import EmptyState from './components/EmptyState'
 import ListingCard from './components/listings/ListingCard'
 
-interface IHome {
+interface HomeProps {
   searchParams: IListingsParams
 }
 
-const Home = async ({ searchParams }: IHome) => {
-  const ligstings = await getListings(searchParams)
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams)
   const currentUser = await getCurrentUser()
 
-  if (ligstings.length === 0) {
+  if (listings.length === 0) {
     return (
       <ClientOnly>
         <EmptyState showReset />
       </ClientOnly>
     )
   }
+
   return (
     <ClientOnly>
       <Container>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {ligstings.map((listing) => {
-            return (
-              <ListingCard
-                key={listing.id}
-                currentUser={currentUser}
-                data={listing}
-              />
-            )
-          })}
+        <div
+          className="
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+        >
+          {listings.map((listing: any) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))}
         </div>
       </Container>
     </ClientOnly>
