51ec0eba7a7fc1ec2510b4f97c6f6768cac8f369
Zheka <zheseleznev@yandex.ru>
2024-02-05 20:56:32 +0300

    Update seatch component and craete middleware

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,21 +1,60 @@
 'use client'
+import useCities from '@/hooks/useCities'
 import useSearchModal from '@/hooks/useSearchModal'
+import { differenceInDays } from 'date-fns'
+import { useSearchParams } from 'next/navigation'
+import { useMemo } from 'react'
 import { BiSearch } from 'react-icons/bi'
+import { start } from 'repl'
 
 const Search = () => {
   const searchModal = useSearchModal()
+  const params = useSearchParams()
+  const { getByValue } = useCities()
+
+  const locationValue = params?.get('locationValue')
+  const startDate = params?.get('startDate')
+  const endDate = params?.get('endDate')
+  const guestCount = params?.get('guestCount')
+
+  const locationLabel = useMemo(() => {
+    if (locationValue) {
+      return getByValue(locationValue as string)?.value
+    }
+    return 'Где-угодно'
+  }, [getByValue, locationValue])
+
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const start = new Date(startDate as string)
+      const end = new Date(endDate as string)
+      let diff = differenceInDays(end, start)
+      if (diff === 0) {
+        diff = 1
+      }
+      return `${diff} дней`
+    }
+    return 'Каждый день'
+  }, [startDate, endDate])
+
+  const guestLabel = useMemo(() => {
+    if (guestCount) {
+      return `Гостей: ${guestCount}`
+    }
+    return 'Все гости'
+  }, [guestCount])
   return (
     <div
       onClick={searchModal.onOpen}
       className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer"
     >
       <div className="flex flex-row items-center justify-between">
-        <div className="text-sm font-semibold px-6">Где-угодно</div>
+        <div className="text-sm font-semibold px-6">{locationLabel}</div>
         <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
-          Каждую неделю
+          {durationLabel}
         </div>
         <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
-          <div className="hidden sm:block">Добавить гостя</div>
+          <div className="hidden sm:block">{guestLabel}</div>
           <div className="p-2 bg-black rounded-full text-white">
             <BiSearch size={18} />
           </div>
