f5919f13c692f3501885bb5996428b965bed270b
DanilGrebnev <t902hda@mail.ru>
2024-06-30 11:27:46 +0300

    feat: delete paymants

--- payments/apps/payment_accounts/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class PaymentAccountsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.payment_accounts'
-    verbose_name = 'Счета пользователя'
--- payments/apps/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import include, path
-
-urlpatterns = [
-    path('payment_accounts/', include('apps.payment_accounts.urls')),
-    path('transactions/', include('apps.transactions.urls')),
-]
