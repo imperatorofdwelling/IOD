982959178b22c0bc8e4aeae1a88df65fc7c31bad
Shairgul Imarova <shairgul.imarova.kk@gmail.com>
2024-09-12 11:55:50 +0600

    Used clsx to concat the classes

--- shared/ui/components/InputField.tsx
+++ shared/ui/components/InputField.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import clsx from 'clsx';
 
 interface InputFieldProps {
   placeholder: string;
@@ -6,6 +7,11 @@ interface InputFieldProps {
 }
 
 const InputField: React.FC<InputFieldProps> = ({ placeholder, type }) => {
+  
+  const inputClass = clsx(
+    "gap-2.5 self-stretch px-4 py-5 w-full text-sm whitespace-nowrap rounded-lg border border-solid bg-zinc-900 border-zinc-800 min-h-[56px] text-neutral-500"
+  )
+  
   return (
     <div className="mt-2.5 w-full">
       <label htmlFor={`input-${placeholder.toLowerCase()}`} className="sr-only">{placeholder}</label>
@@ -13,7 +19,7 @@ const InputField: React.FC<InputFieldProps> = ({ placeholder, type }) => {
         id={`input-${placeholder.toLowerCase()}`}
         type={type}
         placeholder={placeholder}
-        className="gap-2.5 self-stretch px-4 py-5 w-full text-sm whitespace-nowrap rounded-lg border border-solid bg-zinc-900 border-zinc-800 min-h-[56px] text-neutral-500"
+        className={inputClass}
       />
     </div>
   );
